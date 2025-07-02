6ea5b986adbf3ad41330eec5099bd9248561ad00
Sada.Aljondi <sadaaljondi@hotmail.com>
2025-07-02 12:29:50 -0400

    test

--- __test__/appOperation.test.js
+++ __test__/appOperation.test.js
@@ -3,3 +3,6 @@ const appOperations = require('../src/appOperations');
 test("multiplication property of zero", ()=> {
     expect(appOperations(5, 0)).toBe(0);
 });
+test("adding two valeus", () => {
+    expect(appOperations.add(5, 5)).toBe(10);
+});
