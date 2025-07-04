e9afc5ccaa862605b9cdb6b0170f5f1189128666
Sada.Aljondi <sadaaljondi@hotmail.com>
2025-07-04 14:37:42 -0400

    added WF to publish pkg and fixed add function

--- src/appOperations.js
+++ src/appOperations.js
@@ -3,10 +3,12 @@ function multiply(nmu1, num2){
 }
 
 function add(num1, num2){
-
+    const intNum1 = parseInt(num1, 10);
+    const intNum2 = parseInt(num2, 10);
     return num1 + num2;
 }
 function subtract(num1, num2){
+   
     return num1 - num2;
 }
 function divide(num1, num2){
