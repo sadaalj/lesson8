9e73f0da906470b61e0a93176a4c211cd5f548ee
Sada.Aljondi <sadaaljondi@hotmail.com>
2025-07-05 11:23:07 -0400

    add wf to publish pkg and fixed add function

--- src/appOperations.js
+++ src/appOperations.js
@@ -3,7 +3,7 @@ function multiply(nmu1, num2){
 }
 
 function add(num1, num2){
-    const intNum1 = parseInt(num1, 10);
+    const intNum1 = parseInt(num1, 10); //This line converts the value of num1 to an integer (whole number) using base 10 (decimal system)
     const intNum2 = parseInt(num2, 10);
     return num1 + num2;
 }
